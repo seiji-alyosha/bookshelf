631affa2de1836fcdd48c26f77acb0d237a99426
Jake Tanis <jakktan@gmail.com>
2024-07-08 20:25:36 -0500

    Edited the name of the flask instance to be bookshelf instead of 'app'.
    nothing broke :,)

--- bookshelf/__init__.py
+++ bookshelf/__init__.py
@@ -7,30 +7,30 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    bookshelf = Flask(__name__, instance_relative_config=True)
+    bookshelf.config.from_mapping(
         SECRET_KEY='dev',
         #stores the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, 'bookshelf.sqlite'),
+        DATABASE=os.path.join(bookshelf.instance_path, 'bookshelf.sqlite'),
     )
 
     if test_config is None:
         #to update the secret key for deployment
-        app.config.from_pyfile('config.py', silent=True)
+        bookshelf.config.from_pyfile('config.py', silent=True)
 
     else:
         #configurations for testing
-        app.config.update(test_config)
+        bookshelf.config.update(test_config)
 
     #makes the instance folder if not present.
     #needed because Flask does not make one automatically.
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(bookshelf.instance_path)
     except OSError:
         pass
 
-    @app.route('/')
+    @bookshelf.route('/')
     def hello():
         return 'Testing the application factory.'
     
-    return app
+    return bookshelf
